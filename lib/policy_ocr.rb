03bce4d84222dcf48ae8a3322a28b8deb8790107
Victoria Tiller <vijotiller@gmail.com>
2021-11-10 14:43:38 -0500

    can check for 1 and 4 as first number

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -10,17 +10,25 @@ module PolicyOcr
         i = 0
         j = 0
         # look at first line
-        while j < 3
-            # Look at first “chunk” of 3 characters.
+      
+            # Look at first “chunk” of 3 characters. 
             # last chunk  is 24..26
-            while i <= 24 do
+        while i <= 24 do
+            # First chunk: j = 0, i = 0..2
             # If the chunk is “   ”
+            if file_data[j][i..(i+2)] == "   "
             # Number is 1 or 4
             # Check 2nd line (same position of chunk)
-            # If chunk is “|_|”
-            # Number is 4. Proceed.
-            # If chunk is “  |”
-            # Number is 1. Proceed.
+                # If chunk is “|_|”
+                if file_data[j+1][(i..i+2)] == "|_|"
+                    # Number is 4. Proceed.
+                    answer += "4"
+                    # If chunk is “  |”
+                else
+                    # Number is 1. Proceed.
+                    number += "1"
+                end
+            else
             # If the chunk is “ _ “
             # Number is a 0, 2, 3, 5, 6, 7, 8, or 9
             # Check 2nd line (of that chunk)
@@ -48,14 +56,17 @@ module PolicyOcr
             # Number is 5. Proceed.
             # If chunk is “|_|”
             # Number is 6. Proceed.
+                
+            #end if first_line loop
+            end
                   
     # Repeat with other 8 “chunks”
-                i += 3
-            end
-            j += 1
+            i += 3
+        #end while i loop
         end
+      
 # Skip a line, and repeat until you reach the end.
-
+    #end def
     end
 
 end
