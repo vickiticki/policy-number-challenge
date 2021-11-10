6e124f0bda3aa32c7f4f42d4da7d48b319215d4a
Victoria Tiller <vijotiller@gmail.com>
2021-11-10 15:35:35 -0500

    trying to figure out rspec

--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -8,4 +8,17 @@ describe PolicyOcr do
   it 'loads the sample.txt' do
     expect(fixture('sample').lines.count).to eq(44)
   end
+
+  #describe ".make_numbers" do
+    #context "given 'sample'" do
+      #it "makes policies" do
+        # arr = [
+                # "000000000", "111111111", "222222222", "333333333",
+                # "444444444", "555555555", "666666666", "777777777",
+                # "888888888", "999999999"
+              # ]
+        #expect (PolicyOcr.make_numbers(fixture('sample'))).to eq(arr)
+      #end
+    #end
+  #end
 end
