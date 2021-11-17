00369cf4fb1008a8d4e0db4677d8e8f96e1f6ea3
Victoria Tiller <vijotiller@gmail.com>
2021-11-17 17:53:00 -0500

    interview done

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -1,5 +1,28 @@
 module PolicyOcr
-    
+
+    DIGITS = {
+    " _ | ||_|" => "0",
+    "     |  |" => "1",
+    " _  _||_ " => "2",
+    " _  _| _|" => "3",
+    "   |_|  |" => "4",
+    " _ |_  _|" => "5",
+    " _ |_ |_|" => "6",
+    " _   |  |" => "7",
+    " _ |_||_|" => "8",
+    " _ |_| _|" => "9"
+    }
+
+    def self.check_sum policy
+        num_arr = policy.to_s.chars.reverse.map(&:to_i)
+        # multiply each digit by index + 1 and add them together
+        check = 0 
+        num_arr.each_with_index do |num, i|
+            check += num * (i + 1)
+        end
+        check % 11 == 0
+    end
+
     def self.make_numbers(policies)
         #this gives me an array of each line as as a string
         
@@ -20,32 +43,22 @@ module PolicyOcr
             # Look at first “chunk” of 3 characters
             while i < 27 do
                 # First chunk: j = 0, i = 0..2
+                # first_line = file_data[2][i..(i+2)]
                 first_line = file_data[j][i..(i+2)]
                 second_line = file_data[j+1][i..(i+2)]
                 third_line = file_data[j+2][i..(i+2)]
                 # combine the three strings...
                 code = first_line + second_line + third_line
                 # and use a case to find the right number
-                answer += case code
-                    when " _ | ||_|" then "0"
-                    when "     |  |" then "1"
-                    when " _  _||_ " then "2"
-                    when " _  _| _|" then "3"
-                    when "   |_|  |" then "4"
-                    when " _ |_  _|" then "5"
-                    when " _ |_ |_|" then "6"
-                    when " _   |  |" then "7"
-                    when " _ |_||_|" then "8"
-                    when " _ |_| _|" then "9"
-                    else "?"
-                end
-                
+                new_digit = DIGITS[code] ? DIGITS[code] : "?"
+                answer += new_digit                
                 # Repeat with other 8 “chunks” by moving over by 3
                 i += 3
             #end while i loop
             end
+
             #put the answer in the answer array
-            all_answers << answer.to_i
+            all_answers << answer
             
             # go to next policy
             # first policy starts at line 1, which is file_data[0]
@@ -61,3 +74,6 @@ module PolicyOcr
     end
 #end module
 end
+
+a = PolicyOcr.make_numbers ('./spec/fixtures/sample.txt')
+p a
